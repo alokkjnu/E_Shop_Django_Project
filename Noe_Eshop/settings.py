f241372db9b49cd682e46170b517511a9559a3fe
alokkjnu <ry7069577@gmail.com>
2021-07-25 18:51:28 +0530

    commit

--- Noe_Eshop/settings.py
+++ Noe_Eshop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shop',
     'cart',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
--- Noe_Eshop/urls.py
+++ Noe_Eshop/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cart/',include('cart.urls',namespace='cart')),
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
 ]
 if settings.DEBUG:
